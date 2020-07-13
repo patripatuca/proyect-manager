ba5ef3c820d30035e8ec173d52ccfb9fbc05d20c
afd <afd@alup5.academiapostal.es.local>
2020-07-13 17:37:55 +0200

    correcciones

--- controllers/autenticacion.js
+++ controllers/autenticacion.js
@@ -1,11 +1,12 @@
-const {Usuario} = require('../models')
+const {Usuario} = require('../models');
+const md5 =require('md5');
 
 function login(req, res) {
     //tomamos los datos del formulario
     const{email,password}=req.body;
-    
+ //email y que la huella md5 de la contraseña coincida tambien.   
 //localizar en la BD el usuario
-Usuario.findOne({where: {email, password}})
+Usuario.findOne({where: {email, password:md5(password)}})
     .then(usuario => {
       if (usuario) {
         req.session.usuario = usuario;
