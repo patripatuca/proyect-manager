e6871e0661fff2de79fa5dadb852e84135065667
afd <afd@alup5.academiapostal.es.local>
2020-07-13 20:38:33 +0200

    correcion de relaciones

--- models/index.js
+++ models/index.js
@@ -11,7 +11,7 @@ Usuario.belongsToMany(Proyecto,{through:'participantes'})
 Proyecto.hasMany(Tarea)
 Tarea.belongsToMany(Usuario, {through: 'asignaciones'})
 Tarea.belongsToMany(Usuario, {as: 'intervenciones', through: Intervencion})
-Usuario.hasOne(Rol)
+Usuario.belongsTo(Rol)
 Rol.hasMany(Rol, {as: 'heredados'})
 
 
