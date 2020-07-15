2ee5fdde6f39ac58960ec4a359f20424de73f9c5
afd <afd@alup5.academiapostal.es.local>
2020-07-15 18:36:06 +0200

    creaccion de botones de reloj

--- controllers/tareas.js
+++ controllers/tareas.js
@@ -1,7 +1,25 @@
+const { Tarea, Intervencion, Usuario } = require("../models");
+const moment = require("moment");
+
 function mostrarTarea(req, res) {
     const id = req.params.id;
-    res.send("Tarea " + id);
+    Tarea.findByPk(id, {include: ['intervenciones']})
+    .then(tarea => {
+        res.render('tarea', {
+            tarea: {
+                link: "/tareas/" + tarea.id,
+                estado: tarea.fechaFin? "Finalizado" : "Pendiente",
+                intervenciones: tarea.intervenciones,
+                iniciada: tarea.intervenciones.some(x => x.fin==null),
+                fechaInicio: moment(tarea.fechaInicio).format("DD/MM/YYYY"),
+                fechaFin: tarea.fechaFin && moment(tarea.fechaFin).format("DD/MM/YYYY"),
+                fechaVencimiento: tarea.fechaVencimiento && moment(tarea.fechaVencimiento).format("DD/MM/YYYY")  
+            }
+        })
+    })
 }
+
+
 
 module.exports = {
     mostrarTarea
